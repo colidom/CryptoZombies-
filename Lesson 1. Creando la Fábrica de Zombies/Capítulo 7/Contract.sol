cce6f3d970e3498b0d069374a25da87c5aead846
colidom <colidom@outlook.com>
2022-07-07 22:05:19 +0100

    Declaración de Funciones

---
+++ Lesson 1. Creando la Fábrica de Zombies/Capítulo 7/Contract.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.25;
+
+contract ZombieFactory {
+    uint256 dnaModulus = 10**dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint256 dna;
+    }
+    Zombie[] public zombies;
+
+    function createZombie(string _name, uint256 _dna) {}
+}
