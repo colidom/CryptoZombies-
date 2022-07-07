5ab110b2640abeb4bc0f35531afec0c94176cffe
colidom <colidom@outlook.com>
2022-07-07 22:06:47 +0100

    Trabajando con estructuras y arrays

---
+++ Lesson 1. Creando la Fábrica de Zombies/Capítulo 8/Contract.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.25;
+
+contract ZombieFactory {
+    uint256 dnaModulus = 10**dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint256 dna;
+    }
+    Zombie[] public zombies;
+
+    function createZombie(string _name, uint256 _dna) {
+        zombies.push(Zombie(_name, _dna));
+    }
+}
