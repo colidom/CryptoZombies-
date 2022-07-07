cbc9cb6d1c1c9fd8821125854417707b2f2dc802
colidom <colidom@outlook.com>
2022-07-07 22:09:12 +0100

    Juntándolo Todo

---
+++ Lesson 1. Creando la Fábrica de Zombies/Capítulo 12/Contract.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.25;
+
+contract ZombieFactory {
+    uint256 dnaModulus = 10**dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint256 dna;
+    }
+    Zombie[] public zombies;
+
+    function _createZombie(string _name, uint256 _dna) private {
+        zombies.push(Zombie(_name, _dna));
+    }
+
+    function _generateRandomDna(string _str) private view returns (uint256) {
+        uint256 rand = uint256(keccak256(_str));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string _name) public {
+        uint256 randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
